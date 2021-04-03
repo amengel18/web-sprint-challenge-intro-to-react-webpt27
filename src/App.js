8af1f6d2a2b02ba8e35b2937fb5791e54b56dd27
Angela Engel <amengel.ae@icloud.com>
2021-04-03 11:16:12 -0600

    Working on App

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  const openDetails = id => {
+    setCurrentCharacterId(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentCharacterId(null)
+  }
 
   useEffect(() => {
     axios.get(`https://swapi.dev/api/people/`)
